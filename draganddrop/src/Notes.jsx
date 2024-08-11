60c2902939f0857078c03b9f0c4897a956ab3a79
abhinav2697 <abhinav2697@gmail.com>
2024-08-11 23:51:47 +0530

    new files and features added

---
+++ draganddrop/src/Notes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Notes = () => {
+    const notes = [
+        {
+            id: 1,
+            text:"Check the description"
+        },
+        {
+            id: 2,
+            text:"Hello you are here"
+        }
+    ]
+  
+}
+
+export default Notes
