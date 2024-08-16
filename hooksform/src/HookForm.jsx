b199db71d56d6a069c2976b6969e8e8f22a79cda
abhinav2697 <abhinav2697@gmail.com>
2024-08-16 16:20:58 +0530

    new files and features added

--- hooksform/src/HookForm.jsx
+++ hooksform/src/HookForm.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 const HookForm = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(watch("example"));
   return (
-    <div>HookForm</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("firstName")} />
+      <select {...register("gender")}>
+        <option value="female">female</option>
+        <option value="male">male</option>
+        <option value="other">other</option>
+      </select>
+      <input defaultValue="test" {...register("example")} />
+      <input {...register("exampleRequired", { required: true })} />
+      {errors.exampleRequired && <span>This field is require</span>}
+      <input type="submit"/>
+    </form>
   )
 }
 
