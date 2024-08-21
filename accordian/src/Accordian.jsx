582447519b6d647f89db3da87f029bc94219854b
abhinav2697 <abhinav2697@gmail.com>
2024-08-21 23:59:40 +0530

    new files and features added

--- accordian/src/Accordian.jsx
+++ accordian/src/Accordian.jsx
@@ -6,35 +6,47 @@ const Accordian = () => {
   const [enableMultiSelection, setEnableMultiSelection] = useState(false);
   const [multiple, setMultiple] = useState([]);
 
-    function handleSingleSelection(getCurrentId) {
-        setSelected(getCurrentId===selected?null:getCurrentId);
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
   }
 
-    return (
-      
-      <div className="wrapper">
-        <button>Enable Multi Selection</button>
+  function handleMultiSelection(getCurrentId) {
+    
+  }
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-                  <div onClick={()=>handleSingleSelection(dataItem.id) } className="title">
+              <div
+                onClick={enableMultiSelection?() => handleSingleSelection(dataItem.id):()=>handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.title}</h3>
                 <span>+</span>
-                  </div>
-                  {
-                      selected === dataItem.id ? 
-                          <div className='content'>{dataItem.content}</div>: null
-                  }
+              </div>
+              {
+                enableMultiSelection ? multiple.indexOf(dataItem.id) !== -1 &&
+                  <div className='content'>{dataItem.answer}</div> :
+                  selected === dataItem.id && <div className='content'>{dataItem.answer}</div>
+              }
+              {selected === dataItem.id || multiple.indexOf(dataItem.id)!==-1? (
+                <div className="content">{dataItem.content}</div>
+              ) : null}
             </div>
           ))
         ) : (
           <div>No data found!</div>
         )}
       </div>
-     
     </div>
   );
 };
 
 export default Accordian;
+
+// 30:00
