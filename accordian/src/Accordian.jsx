22e8915c1e3cd59b0533afe60f8088c6cef7889d
abhinav2697 <abhinav2697@gmail.com>
2024-08-22 12:50:16 +0530

    new files and features added

--- accordian/src/Accordian.jsx
+++ accordian/src/Accordian.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import data from './data';
-import './style.css';
+import './style.css';                                         
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
   const [enableMultiSelection, setEnableMultiSelection] = useState(false);
@@ -11,10 +11,19 @@ const Accordian = () => {
   }
 
   function handleMultiSelection(getCurrentId) {
-    
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMultiple);
   }
+
+  console.log(selected, multiple);
   return (
-    <div className="wrapper">
+    <div className="acc-wrapper">
       <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
         Enable Multi Selection
       </button>
@@ -23,30 +32,36 @@ const Accordian = () => {
           data.map((dataItem) => (
             <div className="item">
               <div
-                onClick={enableMultiSelection?() => handleSingleSelection(dataItem.id):()=>handleSingleSelection(dataItem.id)}
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.title}</h3>
                 <span>+</span>
               </div>
-              {
-                enableMultiSelection ? multiple.indexOf(dataItem.id) !== -1 &&
-                  <div className='content'>{dataItem.answer}</div> :
-                  selected === dataItem.id && <div className='content'>{dataItem.answer}</div>
-              }
-              {selected === dataItem.id || multiple.indexOf(dataItem.id)!==-1? (
-                <div className="content">{dataItem.content}</div>
-              ) : null}
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="acc-content ">{dataItem.content}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="acc-content ">{dataItem.content}</div>
+                  )}
+              {/* {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
             </div>
           ))
         ) : (
-          <div>No data found!</div>
+          <div>No data found !</div>
         )}
       </div>
     </div>
   );
-};
+}
 
-export default Accordian;
+export default Accordian
 
-// 30:00
