c97cff8460f1bbe9c7859f6369d5ebbfd7908e46
abhinav2697 <abhinav2697@gmail.com>
2024-08-23 22:51:12 +0530

    new files and features added

---
+++ randomcolor/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RandomColor  from './RandomColor';
+function App() {
+  return (
+    <div className="App">
+     
+    <RandomColor/>
+    </div>
+  );
+}
+
+export default App;
