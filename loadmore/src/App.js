e81337686500b08bafb0155b76cf2aeb3303bac7
abhinav2697 <abhinav2697@gmail.com>
2024-08-26 22:48:24 +0530

    new files and features added

---
+++ loadmore/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoadMore from './LoadMore';
+function App() {
+  return (
+    <div className="App">
+      <LoadMore/>
+    </div>
+  );
+}
+
+export default App;
+
+// 1:23:16
