fff9269614c03bd9ed326bfbb229fd027aa596cc
abhinav2697 <abhinav2697@gmail.com>
2024-08-31 23:57:57 +0530

    new files and features added

---
+++ darkmodelightmode/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import LightDark from './LightDark';
+function App() {
+  return (
+    <div className="App">
+      <LightDark/>
+    </div>
+  );
+}
+
+export default App;
---
+++ darkmodelightmode/src/LightDark.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import useLocalStorage from "./useLocalStorage"
+import "./theme.css"
+const LightDark = () => {
+    const [theme, setTheme] = useLocalStorage("theme", "dark");
+    
+    function handleToggleTheme() {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+  return (
+    <div className="light-dark-mode" data-theme={theme}>
+      <div className="container">
+        <p>Hello World!</p>
+        <button onClick={handleToggleTheme}>Change Theme</button>
+      </div>
+    </div>
+  );
+};
+
+export default LightDark;
