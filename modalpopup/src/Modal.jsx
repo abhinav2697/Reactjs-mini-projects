f12c653b694bbdd7c743b0e6e7668b2b14aa4031
abhinav2697 <abhinav2697@gmail.com>
2024-09-03 16:17:34 +0530

    new files and features added

--- modalpopup/src/Modal.jsx
+++ modalpopup/src/Modal.jsx
@@ -3,29 +3,28 @@ import CustomModal from "./CustomModal";
 import "./modal.css"
 
 const Modal = () => {
-    const [showModal, setShowModal] = useState(false);
+    const [showModalPopup, setShowModalPopup] = useState(false);
 
-    function handleToggleModalPopup() {
-        setShowModal(!showModal);
-    }
-    function onClose() {
-        setShowModal(false);
-    }
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup);
+  }
+
+  function onClose() {
+    setShowModalPopup(false);
+  }
 
   return (
-      <div>
-          <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
-          {
-              showModal && (
-                  <Modal 
-                      id={"custom-id"}
-                      header={<h1>Customized Header</h1>}
-                      footer={<h1>Customized Footer</h1>}
-                      onClose={onClose}
-                      body={<div>Customized body</div>}
-                  />
-              )
-          }
+    <div>
+      <button onClick={handleToggleModalPopup}>Open Modal Popup</button>
+      {showModalPopup && (
+        <CustomModal
+          id={"custom-id"}
+          header={<h1>Customized Header</h1>}
+          footer={<h1>Customized Footer</h1>}
+          onClose={onClose}
+          body={<div>Customized body</div>}
+        />
+      )}
     </div>
   )
 }
