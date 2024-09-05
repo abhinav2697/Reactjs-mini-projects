d401939ae6726b87ca8471f859cff2f4a3fcdb67
abhinav2697 <abhinav2697@gmail.com>
2024-09-05 22:48:58 +0530

    new files and features added

---
+++ windowresize/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Usewindowresize from './Usewindowresize';
+function App() {
+  return (
+    <div className="App">
+      <Usewindowresize/>
+    </div>
+  );
+}
+
+export default App;
