cc5a64cea1b9da8de5ceadb24aa3767ed9720463
abhinav <abhinav2697@gmail.com>
2024-10-01 01:00:19 +0530

    new project added

--- reduxjstoolkit/src/App.js
+++ reduxjstoolkit/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Products from './components/Products';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
--- reduxjstoolkit/src/components/Products.js
+++ reduxjstoolkit/src/components/Products.js
@@ -1,5 +1,6 @@
 import {useState,useEffect}  from 'react'
-import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Products = () => {
     const [products,setProducts]=useState();
@@ -8,14 +9,35 @@ const Products = () => {
         .then(data=>data.json())
         .then(result=>setProducts(result));
     },[])
+
+    const cards=products.map(product=>(
+      <div className='col-mod-3'>
+           <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+      </div>
+    ))
   return (
     <>
-      <h1>Products</h1>
-      {JSON.stringify(products)}
+      <h1>Products Dashboard</h1>
+    <div className='row'>
+      {cards}
+
+    </div>
     </>
   
   )
 }
 
 export default Products
+
+// 15:30
 
