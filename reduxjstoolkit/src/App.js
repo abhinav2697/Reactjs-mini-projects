058d6344a535307ce175e54bbe267b461ef8cd04
abhinav <abhinav2697@gmail.com>
2024-10-02 00:00:07 +0530

    new project added

--- reduxjstoolkit/src/App.js
+++ reduxjstoolkit/src/App.js
@@ -1,8 +1,19 @@
 
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Cart from './components/Cart';
 import Products from './components/Products';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom';
 function App() {
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route>
+ <Route index element={<Dashboard/>}> </Route>
+ <Route element={<Cart/>}></Route>
+    </Route>
+     
+  
+  ))
   return (
     <div className="App">
   <Products/>
--- reduxjstoolkit/src/components/Products.js
+++ reduxjstoolkit/src/components/Products.js
@@ -10,18 +10,21 @@ const Products = () => {
         .then(result=>setProducts(result));
     },[])
 
-    const cards=products.map(product=>(
-      <div className='col-mod-3'>
-           <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    const cards=products?.map((product)=>(
+      <div className="col-md-3" style={{marginBottom:'10px'}}>
+           <Card key={product.id} className='h-100'>
+            <div className='text-center'>
+            <Card.Img variant="top" src={product.image} style={{width:'100px',height:'130px'}} />
+            </div>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{product.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          INR:{product.price}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
+      <Card.Footer style={{background:'white'}}>
+        <Button variant="primary">Add To Cart</Button>
+        </Card.Footer>
     </Card>
       </div>
     ))
