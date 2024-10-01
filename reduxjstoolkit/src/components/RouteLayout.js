147df1932c991dbb7f600b777c69ac905cdff3e4
abhinav <abhinav2697@gmail.com>
2024-10-02 00:39:24 +0530

    new project added

--- reduxjstoolkit/src/components/RouteLayout.js
+++ reduxjstoolkit/src/components/RouteLayout.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import React from 'react'
 
+
 const RouteLayout = () => {
   return (
     <>
+
     <h1>Navigation</h1>
     <main>  <Outlet/></main>
   
