987e22734c4a72f09c826e585a22f58cab6eff4e
abhinav <abhinav2697@gmail.com>
2024-10-04 00:59:32 +0530

    new project added

--- reduxjstoolkit/src/components/RouteLayout.js
+++ reduxjstoolkit/src/components/RouteLayout.js
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import React from 'react'
 import NavBar from './NavBar'
+import {Provider} from 'react-redux';
+import store from '../store/store';
 
 const RouteLayout = () => {
   return (
     <>
+    <Provider store={store}>
 <NavBar/>
     <main>  
       <Outlet/>
       </main>
+      </Provider>
     </>
   )
 }
