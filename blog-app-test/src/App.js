d9d14d3e1e4b390a78e80ef6e2b3568b0bd9cbab
abhinav <abhinav2697@gmail.com>
2024-10-15 00:49:31 +0530

    new changes  added

--- blog-app-test/src/App.js
+++ blog-app-test/src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>First React Test Case</p>
+      <p>First React Test case</p>
+      <img title='MY Desk' src='https://miro.medium.com/v2/resize:fit:1100/format:webp/1*TKfRZ51S8wtYNNvZwGiOMA.jpeg'/>
     </div>
   );
 }
 
 export default App;
+
+
--- blog-app-test/src/App.test.js
+++ blog-app-test/src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test("Test First React App case",()=>{
   render(<App/>);
-  const text=screen.getByText("First React Test Case");
+  const text=screen.getByText(/First React Test Case/i);
+  const title=screen.getByTitle('MY Desk')
+  expect(title).toBeInTheDocument();
   expect(text).toBeInTheDocument();
 })
