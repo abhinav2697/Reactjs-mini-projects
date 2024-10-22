ef486c0a96473db694ed3762955ba632a86fb979
abhinav <abhinav2697@gmail.com>
2024-10-22 23:58:13 +0530

    new changes  added

---
+++ blog-app-test/src/Fc.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Fc = () => {
+    const [data,setData]=useState("");
+  return (
+    <div className='App'>
+        <h1>Functional Component method testing</h1>
+    </div>
+  )
+}
+
+export default Fc
