968d0f36c92156e057745d59ae81bbeeb3a97371
abhinav <abhinav2697@gmail.com>
2024-10-25 00:48:03 +0530

    new changes  added

--- blog-app-test/src/GetAllByRole.js
+++ blog-app-test/src/GetAllByRole.js
@@ -6,6 +6,17 @@ const GetAllByRole = () => {
         <h1>RTL Query:GetAllByRole</h1>
         <button>Click Me</button>
         <button>Click Me</button>
+        <button>Click Me</button>
+        <button>Click Me</button>
+        <button>Click Me</button>
+        <button>Click Me</button>
+
+        <select>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+            <option>Option 4</option>
+        </select>
         </div>
   )
 }
--- blog-app-test/src/GetAllByRole.test.js
+++ blog-app-test/src/GetAllByRole.test.js
@@ -4,6 +4,16 @@ import {render,screen} from '@testing-library/react';
 test("getAllByRole",()=>{
     render(<GetAllByRole/>)
     const btns=screen.getAllByRole("button");
+    const select=screen.getAllByRole("option");
     expect(btns[1]).toBeInTheDocument();
+    for(let  i=0;i<btns.length;i++){
+        expect(btns[i]).toBeInTheDocument();
+    }
+
+    for(let i=0;i<select.length;i++){
+        expect(select[i]).toBeInTheDocument();
+    }
+
+    
 
 })
