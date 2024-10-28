25ba4a0057d08f35f642bb236c4c1e1e0999336b
abhinav <abhinav2697@gmail.com>
2024-10-29 00:45:36 +0530

    new changes  added

---
+++ blog-app-test/src/GetByText.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GetByText = () => {
+  return (
+    <div>
+        <h1>RTL Query: getByText </h1>
+        <button>Login</button>
+        <p>P tag testing</p>
+        <h1>h1 test</h1>
+        <h1>h1 test</h1>
+    </div>
+  )
+}
+
+export default GetByText
---
+++ blog-app-test/src/GetByText.test.js
@@ -0,0 +1,29 @@
+import { render,screen } from "@testing-library/react";
+import GetByText from "./GetByText";
+
+test("single button testing",()=>{
+    render(<GetByText/>);
+    const btn=screen.getByText("Login");
+    expect(btn).toBeInTheDocument();
+})
+
+test("single p tag testing",()=>{
+    render(<GetByText/>);
+    const ptag=screen.getByText("P tag testing");
+    expect(ptag).toBeInTheDocument();
+
+})
+
+// test("single h1 tag testing",()=>{
+//     render(<GetByText/>);
+//     const head=screen.getByText("h1 test");
+//     expect(head).toBeInTheDocument();
+
+// })
+
+test("multiple h1 tag testing",()=>{
+    render(<GetByText/>);
+    const head=screen.getAllByText("h1 test");
+    expect(head[1]).toBeInTheDocument();
+
+})
