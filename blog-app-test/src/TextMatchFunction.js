d828e484926ac359d573f6e23feed5f3af11b67a
abhinav <abhinav2697@gmail.com>
2024-11-02 00:16:15 +0530

    new changes  added

---
+++ blog-app-test/src/TextMatchFunction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TextMatchFunction = () => {
+  return (
+    <div>
+        <h1>TextMatchFunction</h1>
+        <div>Hello World</div>
+    </div>
+  )
+}
+
+export default TextMatchFunction
---
+++ blog-app-test/src/TextMatchFunction.test.js
@@ -0,0 +1,9 @@
+import { render,screen } from "@testing-library/react";
+import TextMatchFunction from "./TextMatchFunction";
+test("test match with function",()=>{
+    render(<TextMatchFunction/>);
+    const dv=screen.getByText((content)=>{
+        
+    })
+
+})
