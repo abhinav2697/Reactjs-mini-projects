52d98bb6466c8fcef8c7afdfdb0b3ef82f3b7cb1
abhinav <abhinav2697@gmail.com>
2024-11-02 23:54:24 +0530

    new changes  added

--- blog-app-test/src/TextMatchFunction.test.js
+++ blog-app-test/src/TextMatchFunction.test.js
@@ -2,7 +2,7 @@ import { render,screen } from "@testing-library/react";
 import TextMatchFunction from "./TextMatchFunction";
 test("test match with function",()=>{
     render(<TextMatchFunction/>);
-    const dv=screen.getByText((content)=>{
+    const dv=screen.getByText((content,element)=>content.startsWith("Hello"{
         
     })
 
