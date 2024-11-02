33e41ae477f90848bbd18258c470c91ec706264b
abhinav <abhinav2697@gmail.com>
2024-11-03 00:37:55 +0530

    new changes  added

--- blog-app-test/src/TextMatchFunction.test.js
+++ blog-app-test/src/TextMatchFunction.test.js
@@ -2,8 +2,11 @@ import { render,screen } from "@testing-library/react";
 import TextMatchFunction from "./TextMatchFunction";
 test("test match with function",()=>{
     render(<TextMatchFunction/>);
-    const dv=screen.getByText((content,element)=>content.startsWith("Hello"))
-    expect(dv).toBeInTheDocument();
+    // const dv=screen.getByText((content,element)=>content.startsWith("Hello"))
+    //  const dv=screen.getByText((content,element)=>content.endsWith("World"))
+    // const dv=screen.getByText((content,element)=>content.endsWith("ld"))
+    const dv=screen.getByText((content,element)=>content.includes("ld"))
+    // expect(dv).toBeInTheDocument();
         
     })
 
