5983bd6ae766970a415f6dae3519618b69616b44
abhinav <abhinav2697@gmail.com>
2024-11-03 00:58:59 +0530

    new changes  added

--- blog-app-test/src/QueryBy.js
+++ blog-app-test/src/QueryBy.js
@@ -5,8 +5,10 @@ const QueryBy = () => {
   return (
     <div>
         <h1>QueryBy</h1>
-        <button>Login</button>
-        <button>Logout</button>
+        {
+            login? <button onClick={() => {login=false;}}>Logout</button> : <button onClick={() => {login=true;}}>Login</button>  // Toggle Login/Logout button based on login status.  // The button is disabled when login is true.  // The button text changes to Logout when login is true.  // The button text changes to Login when login is false.  // The button text is determined by the value of login.  // The button is disabled when login is true.  // The button text changes to Logout when login is true.  // The button text changes to Login when login is false.  // The button text is determined by the value of login.  // The button is disabled when login is true.  // The button text changes to Logout when login is true.  // The button text changes to Login when login is false.  // The button text is determined by the value of login.  // The button is
+        }
+       
     
     </div>
   )
